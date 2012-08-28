e5120b46222629daadbbd37cfcbc031cfc0d3c63
Anatoly Larin <me@alarin.ru>
2012-08-28 19:55:39 +0400

    [+] login/logout

--- apps/carrot_dash/urls.py
+++ apps/carrot_dash/urls.py
@@ -1,4 +1,6 @@
+import django
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import logout
 
 urlpatterns = patterns('carrot_dash.views',
     url(r'^$', 'home', name='carrot_home'),
@@ -6,4 +8,7 @@ urlpatterns = patterns('carrot_dash.views',
     url(r'^/dash/pm$', 'dash_pm', name='dash_pm'),
     url(r'^/dash/qa$', 'dash_qa', name='dash_qa'),
     url(r'^/dash/designer$', 'dash_designer', name='dash_designer'),
+
+    url(r'^logout/$', 'logout_view', name='carrot_logout'),
+    url(r'^login/$', 'login_view', name='carrol_login')
 )
--- apps/carrot_dash/views.py
+++ apps/carrot_dash/views.py
@@ -1,15 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout, login
 from carrot_dash.models import Roles
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 
 
+@login_required
 def home(request):
     if Roles.has_role(request.user, Roles.developer):
         return dash_developer(request)
     return TemplateResponse(request, 'carrot/site_base.html', {})
 
 
+@login_required
 def dash_developer(request):
     project = request.user.carrotprofile.projects.all()[0]
     versions = project.version_set.filter(is_completed=False).order_by('end_date')
@@ -33,3 +38,20 @@ def dash_qa(request):
 
 def dash_pm(request):
     return HttpResponse('Not realized yet')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('carrot_home')
+
+
+def login_view(request):
+    login_form = AuthenticationForm(request, request.POST or None)
+    request.session.set_test_cookie()
+    if login_form.is_valid():
+        login(request, login_form.get_user())
+        return redirect('carrot_home')
+    data = {
+        'login_form': login_form,
+    }
+    return TemplateResponse(request, 'carrot/dash/login.html', data)
