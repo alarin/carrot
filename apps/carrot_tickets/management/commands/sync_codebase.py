c1519d199ec137eb2f3ef121980ae196b7902c2c
Anatoly Larin <me@alarin.ru>
2012-08-28 21:06:56 +0400

    [*] download attachments from Codebase

--- apps/carrot_tickets/management/commands/sync_codebase.py
+++ apps/carrot_tickets/management/commands/sync_codebase.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals
+import urllib2
+import urllib
+from django.core.files.base import ContentFile
+from django.conf import settings
 from carrot_timetrack.models import TicketEstimate
 from django.contrib.auth.models import User
 from carrot_tickets.codebase import tickets
-from carrot_tickets.models import Version, Ticket
+from carrot_tickets.models import Version, Ticket, TicketAttachment
 from carrot_tickets import codebase
 
 from django.core.management.base import BaseCommand
@@ -35,8 +39,7 @@ class Command(BaseCommand):
                 'number': cbt['ticket_id']
             }
             if notes:
-                options['description'] = notes[0]['ticket_note']['content']
-
+                options['description'] = '\n'.join(note['ticket_note']['content'] for note in notes)
 
             options.update(common_options)
             ticket = Ticket.objects.filter(**common_options).filter(summary=options['summary'])
@@ -48,10 +51,27 @@ class Command(BaseCommand):
             for k, v in options.items():
                 setattr(ticket, k, v)
             ticket.save()
+
+            #login to download attach
+            cookie_handler = urllib2.HTTPCookieProcessor()
+            opener = urllib2.build_opener(cookie_handler)
+            resp = opener.open(settings.CARROT_CODEBASE_LOGIN_URL,
+                    urllib.urlencode({'username': settings.CARROT_CODEBASE_LOGIN,
+                     'password': settings.CARROT_CODEBASE_PASSWORD}))
+
+            for note in notes:
+                attach = note['ticket_note'].get('attachment', {})
+                if attach and attach.get('url'):
+                    file_obj, created \
+                        = TicketAttachment.objects.get_or_create(name=attach['file-name'], ticket=ticket)
+                    content = opener.open(attach['url']).read()
+                    file_obj.file.save(attach['file-name'], ContentFile(content), save=False)
+                    file_obj.save()
+
             if cbt.get('estimated_time'):
-                te, created = TicketEstimate.objects.get_or_create(ticket=ticket)
-                te.hours = int(cbt.get('estimated_time')/60)
-                te.save()
+                    te, created = TicketEstimate.objects.get_or_create(ticket=ticket)
+                    te.hours = int(cbt.get('estimated_time')/60)
+                    te.save()
             print ticket
 
 
--- apps/carrot_tickets/settings.py
+++ apps/carrot_tickets/settings.py
@@ -2,3 +2,8 @@ AUTOSLUG_SLUGIFY_FUNCTION = 'carrot_tickets.utils.slugify'
 
 CARROT_CODEBASE_USERNAME = 'streamlet/tony_larin'
 CARROT_CODEBASE_KEY = 'e76e6a2da473a1f6ba4d69f544747f1fcbfd4fdf'
+
+##for attachments
+CARROT_CODEBASE_LOGIN_URL = 'https://streamlet.codebasehq.com/login'
+CARROT_CODEBASE_LOGIN = 'tony_larin'
+CARROT_CODEBASE_PASSWORD = 'codebase_12_12'
