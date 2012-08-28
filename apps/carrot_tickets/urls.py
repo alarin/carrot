550d16bf4947352bcf7855c81b7c3fd94b3cb692
Anatoly Larin <me@alarin.ru>
2012-08-28 21:29:50 +0400

    [-] project slug url fixed

--- apps/carrot_tickets/urls.py
+++ apps/carrot_tickets/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('carrot_tickets.views',
-    url(r'^(?P<project_slug>[\w\d_]+)/(?P<ticket_number>\d+)/$', 'ticket', name='carrot_ticket'),
+    url(r'^(?P<project_slug>[\w\d_-]+)/(?P<ticket_number>\d+)/$', 'ticket', name='carrot_ticket'),
 )
