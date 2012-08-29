746cb963d31d85b50db915a230e3ecc7bd8cf89f
Anatoly Larin <me@alarin.ru>
2012-08-29 13:43:31 +0400

    [+] ticket edit form

--- apps/carrot_tickets/forms.py
+++ apps/carrot_tickets/forms.py
@@ -1,11 +1,17 @@
 from carrot_tickets.core_forms import CoreFormMixin
-from carrot_tickets.models import TicketComment
+from carrot_tickets.models import TicketComment, Ticket
 from django import forms
 
 class CommentForm(CoreFormMixin, forms.ModelForm):
     class Meta:
         model = TicketComment
         fields = ('content',)
+
+
+class TicketForm(CoreFormMixin, forms.ModelForm):
+    class Meta:
+        model = Ticket
+        exclude = ('reporter',)
 
 
 
--- apps/carrot_tickets/views.py
+++ apps/carrot_tickets/views.py
@@ -1,5 +1,5 @@
 #encoding: utf-8
-from carrot_tickets.forms import CommentForm
+from carrot_tickets.forms import CommentForm, TicketForm
 from carrot_tickets.workflow import get_actions, apply_action
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -46,3 +46,34 @@ def ticket(request, project_slug, ticket_number):
         'actions': actions,
     }
     return TemplateResponse(request, 'carrot/tickets/ticket.html', data)
+
+
+@login_required
+def ticket_edit(request, project_slug, ticket_number):
+    ticket = get_object_or_404(Ticket, number=ticket_number, project__slug=project_slug)
+    images = ticket.attachments.filter(kind='image').order_by('created')
+    files = ticket.attachments.exclude(kind='image').order_by('created')
+
+    ticket_form = TicketForm(instance=ticket)
+
+    if request.method == 'POST':
+        redirect_path = request.POST.get('next') or\
+            redirect('carrot_ticket', project_slug=ticket.project.slug, ticket_number=ticket.number)
+        if request.POST.get('action', 'cancel') == 'cancel':
+            return redirect_path
+        if request.POST.get('action') == 'save':
+            ticket_form = TicketForm(request.POST, instance=ticket)
+            if ticket_form.is_valid():
+                ticket = ticket_form.save(commit=False)
+                if not ticket.reporter:
+                    ticket.reporter = request.user
+                ticket.save()
+            return redirect_path
+
+    data = {
+        'ticket': ticket,
+        'files': files,
+        'images': images,
+        'ticket_form': ticket_form,
+    }
+    return TemplateResponse(request, 'carrot/tickets/ticket_edit.html', data)
