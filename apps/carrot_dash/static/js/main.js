71a2bfc700a2d03afb378a0715dda826c63a79fb
Anatoly Larin <me@alarin.ru>
2012-10-19 10:33:26 +0400

    [-] hide fixed filter

--- apps/carrot_dash/static/js/main.js
+++ apps/carrot_dash/static/js/main.js
@@ -13,5 +13,75 @@ $(function(){
         var $input = $('<input type="hidden" name="hours" value="' + hours + '">')
         $(this).closest('form').append($input);
         return true;
-    })
+    });
+
+
+    Carrot.FixedFilter.init();
 });
+
+Carrot.FixedFilter = {
+    init: function() {
+        if ($('.js_fixed_filter').length)
+        {
+            var filter = $('<input class="fixed_filter" type="checkbox" id="fixed_filter">' +
+                '<label for="fixed_filter">скрыть выполненные</label>')
+
+            $('.js_fixed_filter').append(filter);
+            $(filter[0]).change(function(){
+                Carrot.FixedFilter.apply(this.checked);
+            });
+
+            if (Carrot.FixedFilter.load()) {
+                $(filter[0]).attr('checked', 'checked');
+            }
+            $(filter[0]).change();
+        }
+    },
+
+    apply: function(hide)
+    {
+        var tickets = $('.tickets tr.status-fixed, .tickets tr.status-rejected');
+
+        if (hide) {
+            tickets.hide();
+        } else {
+            tickets.show();
+        };
+
+        Carrot.FixedFilter.save(hide);
+    },
+
+    save: function(hide)
+    {
+        setCookie('fixed_filter', hide);
+    },
+
+    load: function()
+    {
+        return getCookie('fixed_filter') == 'true';
+    }
+}
+
+function setCookie (name, value) {
+    document.cookie = name + "=" + escape(value)
+}
+
+function getCookie(name) {
+    var cookie = " " + document.cookie;
+    var search = " " + name + "=";
+    var setStr = null;
+    var offset = 0;
+    var end = 0;
+    if (cookie.length > 0) {
+        offset = cookie.indexOf(search);
+        if (offset != -1) {
+            offset += search.length;
+            end = cookie.indexOf(";", offset)
+            if (end == -1) {
+                end = cookie.length;
+            }
+            setStr = unescape(cookie.substring(offset, end));
+        }
+    }
+    return(setStr);
+}
